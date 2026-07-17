30ac3de16983d10d4bf7b8d6af7f146a788205ee
saranginath <saranginath1996@gmail.com>
2026-07-17 14:18:43 +0400

    modified changes

--- sarangi_portfolio/src/components/Contact.tsx
+++ sarangi_portfolio/src/components/Contact.tsx
@@ -25,19 +25,24 @@ function Contact() {
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         setLoading(true);
+
         try {
             await emailjs.send(
                 import.meta.env.VITE_EMAILJS_SERVICE_ID,
                 import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
                 {
-                    from_name: form.name,
-                    from_email: form.email,
-                    message: form.message
-                }, {
-                publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
-            })
+                    name: form.name,
+                    email: form.email,
+                    message: form.message,
+                    time: new Date().toLocaleString(),
+                },
+                {
+                    publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
+                }
+            );
 
-            toast.success("Message sent successfully!")
+            toast.success("Message sent successfully!");
+
             setForm({
                 name: "",
                 email: "",
@@ -45,14 +50,11 @@ function Contact() {
             });
 
         } catch (error) {
+            toast.error("Failed to send message",);
+        } finally {
             setLoading(false);
-            toast.error("Failed to send message");
         }
-        finally {
-            setLoading(false)
-        }
-    }
-
+    };
     return (
         <section id="contact" className="max-w-6xl mx-auto px-6 py-16">
             < h1 className="text-3xl font-bold text-center mb-10" >
